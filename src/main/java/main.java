71252cf2077341926c29ff50b012c3edb1598796
Kuchiishi Shousei <e195757@ie.u-ryukyu.ac.jp>
2019-12-09 23:47:58 +0900

    report4
    step3

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,12 +1,19 @@
-public class main {
-    public static void main(String[] args){
-        String str = null;
+/*public class main {
+    *public static void main(String[] args){
+        *String str = null;
 
-        try{
-            System.out.println(str.length());
-        }catch (NullPointerException s){
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.println(s.getMessage());
-        }
+        *try{
+            *System.out.println(str.length());
+        *}catch (NullPointerException s){
+            *System.out.println("NullPointerExceptionが発生しました");
+            *System.out.println(s.getMessage());
+        *}
+    *}
+}*/
+
+public class main{
+    public static void main(String[] args){
+        String str = "百二十三";
+        int value =  Integer.parseInt(str);
     }
 }
